7c35c743da46f821b8d0d76e8af91d54d10b293d
ashkembi <ashkembi@umich.edu>
2022-12-16 02:06:41 -0500

    added loop for NB analysis by year and subunit

--- Code/draft script.R
+++ Code/draft script.R
@@ -53,7 +53,7 @@ tdm_occ <- as.matrix(text2map::dtm_builder(MSHA2, OCCUPATION, DOCUMENT_NO))
 
 trainMatrix <- as.data.frame(tdm_occ)
 tokenlist <- data.frame(X = 1:ncol(trainMatrix), X0 = colnames(tdm_occ))
-trainCategory <- as.data.frame(select(MSHA2, DOCUMENT_NO, CAL_YR, EXTREME))
+trainCategory <- as.data.frame(select(MSHA2, DOCUMENT_NO, CAL_YR, SUBUNIT, EXTREME))
 
 #tdm_occ_final <- cbind(id = rownames(tdm_occ), as.data.frame(tdm_occ))
 
@@ -100,7 +100,7 @@ phinonextreme <- c(sumnonextreme/denomnonextreme)
 
 i <- which((as.vector(sumextreme) > 1) & (as.vector(sumextreme + sumnonextreme - 2) > 4))
 
-logratio <- log(phiextreme/phinonextreme)
+logratio <- phiextreme/phinonextreme
 logratio = sort(logratio, decreasing = TRUE)
 print(logratio)
 
@@ -115,109 +115,180 @@ print(logratio)
 
 ######################## by year
 
-i_2000 <- which(trainCategory$CAL_YR == 2000)
-
-trainMatrix_yr <- trainMatrix[i_2000, ]
-tokenlist_yr <- data.frame(X = 1:ncol(trainMatrix_yr), X0 = colnames(trainMatrix_yr))
-trainCategory_yr <- trainCategory[i_2000, ]
 
 
-##### CONSTANTS
-# get total # of training examples
-numTrain = nrow(trainMatrix_yr)
+years <- 2000:2021
 
-# get Laplace smoothing factor
-V = length(tokenlist_yr$X0)
+year_df <- NULL
 
-# get prior y_hat MLE, # of spam emails divided by total
-y_hat <- plyr::count(trainCategory_yr$EXTREME)$freq[2]/numTrain
+for(i in 1:length(years)) {
+  current_year <- years[i]
+  
+  i_2000 <- which(trainCategory$CAL_YR == current_year)
+  
+  trainMatrix_yr <- trainMatrix[i_2000, ]
+  trainMatrix_yr <- trainMatrix_yr[, colSums(trainMatrix_yr != 0) > 0]
+  tokenlist_yr <- data.frame(X = 1:ncol(trainMatrix_yr), X0 = colnames(trainMatrix_yr))
+  trainCategory_yr <- trainCategory[i_2000, ]
+  
+  
+  ##### CONSTANTS
+  # get total # of training examples
+  numTrain = nrow(trainMatrix_yr)
+  
+  # get Laplace smoothing factor
+  V = length(tokenlist_yr$X0)
+  
+  # get prior y_hat MLE, # of spam emails divided by total
+  y_hat <- plyr::count(trainCategory_yr$EXTREME)$freq[2]/numTrain
+  
+  # create a column for total amount of words per email # had c(-1) in columns
+  trainMatrix_yr$wordcount = rowSums(trainMatrix_yr)
+  
+  # create vectors of spam and non-spam training examples
+  extremevector <- trainMatrix_yr[trainCategory_yr$EXTREME == 1,]
+  nonextremevector <- trainMatrix_yr[trainCategory_yr$EXTREME == 0, ]
+  
+  # make denominators
+  denomextreme = V + sum(extremevector$wordcount)
+  denomnonextreme = V + sum(nonextremevector$wordcount)
+  
+  # get rid of word count columns
+  extremevector$wordcount <- NULL
+  nonextremevector$wordcount <- NULL
+  
+  
+  # make vector of numerators for spam and not spam, do laplace smoothing
+  # first entry is # of occurrences for word X0 in respective category
+  sumextreme <- apply(extremevector, MARGIN = 2, sum) + 1
+  sumnonextreme <- apply(nonextremevector, MARGIN = 2, sum) + 1
+  
+  # make vectors
+  phiextreme <- c(sumextreme/denomextreme)
+  phinonextreme <- c(sumnonextreme/denomnonextreme)
+  
+  
+  ratio <- phiextreme/phinonextreme
+  ratio = sort(ratio, decreasing = TRUE)
+  
+  df_ratio <- tibble(year = current_year, token = names(ratio), ratio = ratio)
+  
+  year_df <- rbind(year_df, df_ratio)
+  
+  print(paste0("Year: ", current_year))
+}
 
-# create a column for total amount of words per email # had c(-1) in columns
-trainMatrix_yr$wordcount = rowSums(trainMatrix_yr)
+names(logratio[1:5])
+  
 
-# create vectors of spam and non-spam training examples
-extremevector <- trainMatrix_yr[trainCategory_yr$EXTREME == 1,]
-nonextremevector <- trainMatrix_yr[trainCategory_yr$EXTREME == 0, ]
+year_df %>% filter(token %in% names(logratio[1:12]) ) %>%  #.$token %>% unique
+  ggplot(aes(x = year, y = log(ratio), color = token)) +
+  geom_line(se = F) +
+  geom_point() +
+  geom_hline(yintercept = 0, linetype = 2) +
+  facet_wrap(~token) +
+  theme_bw()
 
-# make denominators
-denomextreme = V + sum(extremevector$wordcount)
-denomnonextreme = V + sum(nonextremevector$wordcount)
-
-# get rid of word count columns
-extremevector$wordcount <- NULL
-nonextremevector$wordcount <- NULL
+year_df %>% filter(year == 2016) %>% filter(token == "face")
 
 
-# make vector of numerators for spam and not spam, do laplace smoothing
-# first entry is # of occurrences for word X0 in respective category
-sumextreme <- apply(extremevector, MARGIN = 2, sum) + 1
-sumnonextreme <- apply(nonextremevector, MARGIN = 2, sum) + 1
-
-# make vectors
-phiextreme <- c(sumextreme/denomextreme)
-phinonextreme <- c(sumnonextreme/denomnonextreme)
-
-
-logratio <- log(phiextreme/phinonextreme)
-logratio = sort(logratio, decreasing = TRUE)
-print(logratio)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+year_df
 
 
 
 
-
-#fatalities
-MSHA3 <- MSHA2 %>%
-  mutate(
-    NARRATIVE = str_remove_all(NARRATIVE, "[[:punct:]]"),
-    NARRATIVE = str_remove_all(NARRATIVE, "[[:digit:]]+"),
-    NARRATIVE = str_replace_all(NARRATIVE, "[[:space:]]+", " "),
-    NARRATIVE = str_remove_all(NARRATIVE, "[[:space:]]$"),
-    NARRATIVE = tolower(NARRATIVE)
-  )
+######################## by subunit
 
 
-Filter(length, str_extract_all(MSHA2$NARRATIVE, "dead"))
-Filter(length, str_extract_all(MSHA2$NARRATIVE, "death"))
-Filter(length, str_extract_all(MSHA2$NARRATIVE, "fatal"))
-Filter(length, str_extract_all(MSHA2$NARRATIVE, "fatality"))
-Filter(length, str_extract_all(MSHA2$NARRATIVE, "fatality"))
-Filter(length, str_extract_all(MSHA2$NARRATIVE, "fatally"))
-Filter(length, str_extract_all(MSHA2$NARRATIVE, "died"))
+
+subunits <- MSHA2$SUBUNIT %>% unique
+
+subunit_df <- NULL
+
+for(i in 1:length(subunits)) {
+  current_subunit <- subunits[i]
+  
+  i_2000 <- which(trainCategory$SUBUNIT == current_subunit)
+  
+  trainMatrix_yr <- trainMatrix[i_2000, ]
+  trainMatrix_yr <- trainMatrix_yr[, colSums(trainMatrix_yr != 0) > 0]
+  tokenlist_yr <- data.frame(X = 1:ncol(trainMatrix_yr), X0 = colnames(trainMatrix_yr))
+  trainCategory_yr <- trainCategory[i_2000, ]
+  
+  
+  ##### CONSTANTS
+  # get total # of training examples
+  numTrain = nrow(trainMatrix_yr)
+  
+  # get Laplace smoothing factor
+  V = length(tokenlist_yr$X0)
+  
+  # get prior y_hat MLE, # of spam emails divided by total
+  y_hat <- plyr::count(trainCategory_yr$EXTREME)$freq[2]/numTrain
+  
+  # create a column for total amount of words per email # had c(-1) in columns
+  trainMatrix_yr$wordcount = rowSums(trainMatrix_yr)
+  
+  # create vectors of spam and non-spam training examples
+  extremevector <- trainMatrix_yr[trainCategory_yr$EXTREME == 1,]
+  nonextremevector <- trainMatrix_yr[trainCategory_yr$EXTREME == 0, ]
+  
+  # make denominators
+  denomextreme = V + sum(extremevector$wordcount)
+  denomnonextreme = V + sum(nonextremevector$wordcount)
+  
+  # get rid of word count columns
+  extremevector$wordcount <- NULL
+  nonextremevector$wordcount <- NULL
+  
+  
+  # make vector of numerators for spam and not spam, do laplace smoothing
+  # first entry is # of occurrences for word X0 in respective category
+  sumextreme <- apply(extremevector, MARGIN = 2, sum) + 1
+  sumnonextreme <- apply(nonextremevector, MARGIN = 2, sum) + 1
+  
+  # make vectors
+  phiextreme <- c(sumextreme/denomextreme)
+  phinonextreme <- c(sumnonextreme/denomnonextreme)
+  
+  
+  ratio <- phiextreme/phinonextreme
+  ratio = sort(ratio, decreasing = TRUE)
+  
+  df_ratio <- tibble(year = current_subunit, token = names(ratio), ratio = ratio)
+  
+  subunit_df <- rbind(subunit_df, df_ratio)
+  
+  print(paste0("Year: ", current_subunit))
+}
+
+
+
+
+
+subunit_df %>% 
+  group_by(year) %>%
+  slice_max(order_by = ratio, n = 5) %>%
+  ungroup() %>%
+  ggplot(aes(x = fct_reorder(token, ratio), y = log(ratio))) +
+  geom_col() +
+  facet_wrap(~year, scales = "free_y") +
+  coord_flip()
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
