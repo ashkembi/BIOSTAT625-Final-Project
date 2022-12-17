92a0da37619f704eec7362c6b62b2c9aa8a7baeb
ashkembi <ashkembi@umich.edu>
2022-12-17 10:14:40 -0500

    cleaned up draft script. cleaned up rmarkdown. final pdf report done

--- Code/draft script.R
+++ Code/draft script.R
@@ -1,13 +1,15 @@
+# load in the data
 load("/Users/abasshkembi/Dropbox (University of Michigan)/BIOSTAT625 Final Project/BIOSTAT625-Final-Project/Data/MSHA.RData")
 library(tidyverse)
 library(tm)
 
-rmd_files <- list(MSHA2, )
-
-save(MSHA2, year_df, logratio, subunit_df, file = "/Users/abasshkembi/Dropbox (University of Michigan)/BIOSTAT625 Final Project/BIOSTAT625-Final-Project/Data/RMD_data.RData"
+#save data for rmarkdown analysis
+save(MSHA2, year_df, logratio, subunit_df, 
+     file = "/Users/abasshkembi/Dropbox (University of Michigan)/BIOSTAT625 Final Project/BIOSTAT625-Final-Project/Data/RMD_data.RData"
  )
 
 
+# save dataet as tibble
 MSHA <- MSHA %>% as_tibble()
 
 #check the possible injury types
@@ -18,16 +20,10 @@ MSHA %>% group_by(DEGREE_INJURY_CD, DEGREE_INJURY) %>% count()
 MSHA1 <- MSHA %>% filter(!DEGREE_INJURY_CD %in% c('00','08','?')) %>% mutate(EXTREME=as.numeric((DEGREE_INJURY_CD %in% c('01','02'))))
 
 
-#removing 00- accident only; 08 - injuries due to natural causes; and ? - no value found, and extreme cases (fatalities - 01, permanent disability - 02, occupational illness - 07)
-# remove injuries to non-employees since we don't know what type of injury it is (09)
-# making extreme events 03 - days away from work and 04 - days away from work and restricted activity
-#MSHA1 <- MSHA %>% filter(!DEGREE_INJURY_CD %in% c('00','08','?', "01", "02", "07", "09")) %>% mutate(EXTREME=as.numeric((DEGREE_INJURY_CD %in% c('03','04'))))
-
-
-
 #check the possible subunits
 MSHA1 %>% count(SUBUNIT, EXTREME) %>% arrange(-n)
 
+#check the possible degrees of injury
 MSHA1 %>% count(DEGREE_INJURY) %>% arrange(-n)
 
 #check the possible years
@@ -48,6 +44,7 @@ MSHA2 <- MSHA2 %>%
     OCCUPATION = str_replace_all(OCCUPATION, "  {2,}", " ")
   )
 
+#arrange by documebnt no.
 MSHA2 <- MSHA2 %>% arrange(DOCUMENT_NO)
 
 
@@ -59,11 +56,6 @@ tdm_occ <- as.matrix(text2map::dtm_builder(MSHA2, OCCUPATION, DOCUMENT_NO))
 trainMatrix <- as.data.frame(tdm_occ)
 tokenlist <- data.frame(X = 1:ncol(trainMatrix), X0 = colnames(tdm_occ))
 trainCategory <- as.data.frame(select(MSHA2, DOCUMENT_NO, CAL_YR, SUBUNIT, EXTREME))
-
-#tdm_occ_final <- cbind(id = rownames(tdm_occ), as.data.frame(tdm_occ))
-
-colnames(tdm_occ)
-
 
 
 ##### CONSTANTS
@@ -79,35 +71,32 @@ extreme_counts <- plyr::count(trainCategory$EXTREME)
 # add laplace smoother for nonextreme events
 laplace_smoother <- round(extreme_counts$freq[1]/extreme_counts$freq[2])
 
-# create a column for total amount of words per email # had c(-1) in columns
+# create a column for total amount of words per injury report
 trainMatrix$wordcount = rowSums(trainMatrix)
 
-# create vectors of spam and non-spam training examples
+# create vectors of extreme and non-extreme training examples
 extremevector <- trainMatrix[trainCategory$EXTREME == 1,]
 nonextremevector <- trainMatrix[trainCategory$EXTREME == 0, ]
 
 # make denominators
 denomextreme = V + sum(extremevector$wordcount)
-denomnonextreme = V + sum(nonextremevector$wordcount)
+denomnonextreme = V*laplace_smoother + sum(nonextremevector$wordcount)
 
 # get rid of word count columns
 extremevector$wordcount <- NULL
 nonextremevector$wordcount <- NULL
 
 
-# make vector of numerators for spam and not spam, do laplace smoothing
+# make vector of numerators for extreme and non-extreme, do laplace smoothing
 # first entry is # of occurrences for word X0 in respective category
 sumextreme <- apply(extremevector, MARGIN = 2, sum) + 1
 sumnonextreme <- apply(nonextremevector, MARGIN = 2, sum) + laplace_smoother
 
 # make vectors
-phiextreme <- c(sumextreme/(denomextreme + ncol(extremevector)))
-phinonextreme <- c(sumnonextreme/(denomnonextreme + ncol(extremevector)*laplace_smoother))
+phiextreme <- c(sumextreme/denomextreme)
+phinonextreme <- c(sumnonextreme/denomnonextreme)
 
-
-
-#i <- which((as.vector(sumextreme) > 1) & (as.vector(sumextreme + sumnonextreme - 2) > 4))
-
+#note: we chose not to use the log-ratio, just the ratio
 logratio <- phiextreme/phinonextreme
 logratio = sort(logratio, decreasing = TRUE)
 print(logratio)
@@ -122,7 +111,7 @@ print(logratio)
 
 
 ######################## by year
-
+# repeating the analysis for each year
 
 
 years <- 2000:2021
@@ -153,30 +142,30 @@ for(i in 1:length(years)) {
   # add laplace smoother for nonextreme events
   laplace_smoother <- round(extreme_counts$freq[1]/extreme_counts$freq[2])
   
-  # create a column for total amount of words per email # had c(-1) in columns
+  # create a column for total amount of words per injury report
   trainMatrix_yr$wordcount = rowSums(trainMatrix_yr)
   
-  # create vectors of spam and non-spam training examples
+  # create vectors of extreme and non-extreme training examples
   extremevector <- trainMatrix_yr[trainCategory_yr$EXTREME == 1,]
   nonextremevector <- trainMatrix_yr[trainCategory_yr$EXTREME == 0, ]
   
   # make denominators
   denomextreme = V + sum(extremevector$wordcount)
-  denomnonextreme = V + sum(nonextremevector$wordcount)
+  denomnonextreme = V*laplace_smoother + sum(nonextremevector$wordcount)
   
   # get rid of word count columns
   extremevector$wordcount <- NULL
   nonextremevector$wordcount <- NULL
   
   
-  # make vector of numerators for spam and not spam, do laplace smoothing
+  # make vector of numerators for extreme and non-extreme, do laplace smoothing
   # first entry is # of occurrences for word X0 in respective category
   sumextreme <- apply(extremevector, MARGIN = 2, sum) + 1
   sumnonextreme <- apply(nonextremevector, MARGIN = 2, sum) + laplace_smoother
   
   # make vectors
-  phiextreme <- c(sumextreme/(denomextreme + ncol(extremevector)))
-  phinonextreme <- c(sumnonextreme/(denomnonextreme + ncol(extremevector)*laplace_smoother))
+  phiextreme <- c(sumextreme/denomextreme)
+  phinonextreme <- c(sumnonextreme/denomnonextreme)
   
   
   ratio <- phiextreme/phinonextreme
@@ -189,45 +178,14 @@ for(i in 1:length(years)) {
   print(paste0("Year: ", current_year))
 }
 
-names(logratio[1:5])
-  
-
-year_df %>% filter(token %in% names(logratio[1:10]) ) %>%  #.$token %>% unique
-  mutate(token = factor(toupper(token), levels = toupper(names(logratio[1:10])))) %>%
-  ggplot(aes(x = year, y = ratio)) +
-  geom_line() +
-  geom_point(size = 0.6) +
-  stat_smooth(geom = "line", se=F, method = "lm", color = "hotpink", size = 2, alpha = 0.5)+
-  geom_hline(yintercept = 1, linetype = 2) +
-  facet_wrap(~token, ncol = 5) +
-  theme_bw() +
-  labs(x = "Year", y = "Likelihood ratio of extreme injury")
-
-year_df %>% filter(year == 2016) %>% filter(token == "face")
-
-
-year_df %>% 
-  group_by(year) %>%
-  mutate(rank = row_number()) %>%
-  ungroup() %>%
-  filter(token %in% names(logratio[1:10]) ) %>%  #.$token %>% unique
-  ggplot(aes(x = year, y = rank)) +
-  geom_line() +
-  stat_smooth(geom = "line", se=F, method = "lm", color = "hotpink", size = 2, alpha = 0.5)+
-  #geom_point() +
-  scale_y_reverse() +
-  #stat_smooth(geom = "line", se=F, method = "lm", color = "hotpink", size = 2, alpha = 0.5)+
-  #geom_hline(yintercept = 1, linetype = 2) +
-  facet_wrap(~token, ncol = 5) +
-  theme_bw()
 
 
 
 
 ######################## by subunit
+# repeat analaysis for each subunit
 
-
-
+# vector of each subunit
 subunits <- unique(MSHA1[MSHA1$SUBUNIT_CD %in% c(30,1,2,3,17, 6),9])$SUBUNIT
 
 subunit_df <- NULL
@@ -256,30 +214,30 @@ for(i in 1:length(subunits)) {
   # add laplace smoother for nonextreme events
   laplace_smoother <- round(extreme_counts$freq[1]/extreme_counts$freq[2])
   
-  # create a column for total amount of words per email # had c(-1) in columns
+  # create a column for total amount of words per injury report
   trainMatrix_yr$wordcount = rowSums(trainMatrix_yr)
   
-  # create vectors of spam and non-spam training examples
+  # create vectors of extreme and non-extreme training examples
   extremevector <- trainMatrix_yr[trainCategory_yr$EXTREME == 1,]
   nonextremevector <- trainMatrix_yr[trainCategory_yr$EXTREME == 0, ]
   
   # make denominators
   denomextreme = V + sum(extremevector$wordcount)
-  denomnonextreme = V + sum(nonextremevector$wordcount)
+  denomnonextreme = V*laplace_smoother+ sum(nonextremevector$wordcount)
   
   # get rid of word count columns
   extremevector$wordcount <- NULL
   nonextremevector$wordcount <- NULL
   
   
-  # make vector of numerators for spam and not spam, do laplace smoothing
+  # make vector of numerators for extreme and non-extreme, do laplace smoothing
   # first entry is # of occurrences for word X0 in respective category
   sumextreme <- apply(extremevector, MARGIN = 2, sum) + 1
   sumnonextreme <- apply(nonextremevector, MARGIN = 2, sum) + laplace_smoother
   
   # make vectors
-  phiextreme <- c(sumextreme/(denomextreme + ncol(extremevector)))
-  phinonextreme <- c(sumnonextreme/(denomnonextreme + ncol(extremevector)*laplace_smoother))
+  phiextreme <- c(sumextreme/denomextreme)
+  phinonextreme <- c(sumnonextreme/denomnonextreme)
   
   
   ratio <- phiextreme/phinonextreme
@@ -292,16 +250,6 @@ for(i in 1:length(subunits)) {
   print(paste0("Subunit: ", current_subunit))
 }
 
-
-
-subunit_df %>% 
-  group_by(subunit) %>%
-  slice_max(order_by = ratio, n = 5) %>%
-  ungroup() %>%
-  ggplot(aes(x = fct_reorder(token, ratio), y = ratio)) +
-  geom_col() +
-  facet_wrap(~subunit, scales = "free_y") +
-  coord_flip()
   
 
 
@@ -321,119 +269,46 @@ subunit_df %>%
 
 
 
+# run correlations for yearly trend for the top 10 most indicative tokens
 
 
-
-
-
-MSHA2 %>%
-  mutate(SUBUNIT = "Total") %>%
-  group_by(SUBUNIT) %>%
-  summarise(n = n(), extreme = sum(EXTREME)) %>%
-  ungroup() %>%
-  rbind(MSHA2 %>% filter(SUBUNIT %in% subunits) %>% group_by(SUBUNIT) %>% summarise(n = n(), extreme = sum(EXTREME))) %>%
-  rbind(MSHA2 %>% mutate(CAL_YR2 = ceiling((CAL_YR - 1999)/5)) %>% mutate(CAL_YR2 = ifelse(CAL_YR2 > 4, CAL_YR2-1, CAL_YR2)) %>%
-          group_by(CAL_YR2) %>% summarise(n = n(), extreme = sum(EXTREME)) %>% ungroup() %>% rename("SUBUNIT" = "CAL_YR2")
-          ) %>%
-  mutate(perc = round(extreme/n*100, 1)) %>%
-  mutate(extreme = paste0(extreme, " (", perc, "%)")) %>% select(-perc)
-
-
-
-
-logratio[1:10]
-
-sum(logratio > 1)
-length(logratio)
-
-logratio[(length(logratio) - 9):length(logratio)]
-
-
-year_df %>%
-  group_by(year) %>%
-  mutate(rank = row_number()) %>%
-  ungroup() %>%
-  filter(rank == 1) %>%
-  print(n = nrow(.))
-
-
-
-
-
-
-
-
-
-library(lme4)
-library(lmerTest)
-
-year_df %>% filter(token %in% names(logratio[1]) ) %>%
-  lm(ratio ~ year, data = .) %>%
-  summary()
-
-year_df %>% filter(token %in% names(logratio[2]) ) %>%
-  lm(ratio ~ year, data = .) %>%
-  summary()
-
-year_df %>% filter(token %in% names(logratio[3]) ) %>%
-  lm(ratio ~ year, data = .) %>%
-  summary()
-
-year_df %>% filter(token %in% names(logratio[4]) ) %>%
-  lm(ratio ~ year, data = .) %>%
-  summary()
-
-year_df %>% filter(token %in% names(logratio[5]) ) %>%
-  lm(ratio ~ year, data = .) %>%
-  summary()
-
-year_df %>% filter(token %in% names(logratio[6]) ) %>%
-  lm(ratio ~ year, data = .) %>%
-  summary()
-
-year_df %>% filter(token %in% names(logratio[7]) ) %>%
-  lm(ratio ~ year, data = .) %>%
-  summary()
-
-year_df %>% filter(token %in% names(logratio[8]) ) %>%
-  lm(ratio ~ year, data = .) %>%
-  summary()
-
-year_df %>% filter(token %in% names(logratio[9]) ) %>%
-  lm(ratio ~ year, data = .) %>%
-  summary()
-
-year_df %>% filter(token %in% names(logratio[10]) ) %>%
-  lm(ratio ~ year, data = .) %>%
-  summary()
-
+#superintendent
 cor.test(year_df %>% filter(token %in% names(logratio[1]) ) %>% .$year,
     year_df %>% filter(token %in% names(logratio[1]) ) %>% .$ratio)
 
+#auger
 cor.test(year_df %>% filter(token %in% names(logratio[2]) ) %>% .$year,
     year_df %>% filter(token %in% names(logratio[2]) ) %>% .$ratio)
 
+#stoper
 cor.test(year_df %>% filter(token %in% names(logratio[3]) ) %>% .$year,
     year_df %>% filter(token %in% names(logratio[3]) ) %>% .$ratio)
 
+#tender
 cor.test(year_df %>% filter(token %in% names(logratio[4]) ) %>% .$year,
     year_df %>% filter(token %in% names(logratio[4]) ) %>% .$ratio)
 
+#shaftcrew
 cor.test(year_df %>% filter(token %in% names(logratio[5]) ) %>% .$year,
     year_df %>% filter(token %in% names(logratio[5]) ) %>% .$ratio)
 
+#washer
 cor.test(year_df %>% filter(token %in% names(logratio[6]) ) %>% .$year,
     year_df %>% filter(token %in% names(logratio[6]) ) %>% .$ratio)
 
+#shaft
 cor.test(year_df %>% filter(token %in% names(logratio[7]) ) %>% .$year,
     year_df %>% filter(token %in% names(logratio[7]) ) %>% .$ratio)
 
+#iron
 cor.test(year_df %>% filter(token %in% names(logratio[8]) ) %>% .$year,
     year_df %>% filter(token %in% names(logratio[8]) ) %>% .$ratio)
 
+#chute
 cor.test(year_df %>% filter(token %in% names(logratio[9]) ) %>% .$year,
     year_df %>% filter(token %in% names(logratio[9]) ) %>% .$ratio)
 
+#grizzly
 cor.test(year_df %>% filter(token %in% names(logratio[10]) ) %>% .$year,
     year_df %>% filter(token %in% names(logratio[10]) ) %>% .$ratio)
 
