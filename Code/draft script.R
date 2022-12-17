bd4500cf041bd9fa3d3d588d7ec7da331d305010
ashkembi <ashkembi@umich.edu>
2022-12-16 19:07:39 -0500

    added more script for graphs. Added .Rmd for final report

--- Code/draft script.R
+++ Code/draft script.R
@@ -6,7 +6,7 @@ library(tm)
 MSHA <- MSHA %>% as_tibble()
 
 #check the possible injury types
-MSHA %>% select(DEGREE_INJURY_CD, DEGREE_INJURY) %>% distinct()
+MSHA %>% group_by(DEGREE_INJURY_CD, DEGREE_INJURY) %>% count()
 
 #removing 00- accident only; 08 - injuries due to natural causes; and ? - no value found
 # making extreme events 01 - fatalities and 02 - permanent disability
@@ -16,7 +16,7 @@ MSHA1 <- MSHA %>% filter(!DEGREE_INJURY_CD %in% c('00','08','?')) %>% mutate(EXT
 #removing 00- accident only; 08 - injuries due to natural causes; and ? - no value found, and extreme cases (fatalities - 01, permanent disability - 02, occupational illness - 07)
 # remove injuries to non-employees since we don't know what type of injury it is (09)
 # making extreme events 03 - days away from work and 04 - days away from work and restricted activity
-MSHA1 <- MSHA %>% filter(!DEGREE_INJURY_CD %in% c('00','08','?', "01", "02", "07", "09")) %>% mutate(EXTREME=as.numeric((DEGREE_INJURY_CD %in% c('03','04'))))
+#MSHA1 <- MSHA %>% filter(!DEGREE_INJURY_CD %in% c('00','08','?', "01", "02", "07", "09")) %>% mutate(EXTREME=as.numeric((DEGREE_INJURY_CD %in% c('03','04'))))
 
 
 
@@ -68,8 +68,11 @@ numTrain = nrow(trainMatrix)
 # get Laplace smoothing factor
 V = length(tokenlist$X0)
 
-# get prior y_hat MLE, # of spam emails divided by total
-y_hat <- plyr::count(trainCategory$EXTREME)$freq[2]/numTrain
+# count documents
+extreme_counts <- plyr::count(trainCategory$EXTREME)
+
+# add laplace smoother for nonextreme events
+laplace_smoother <- round(extreme_counts$freq[1]/extreme_counts$freq[2])
 
 # create a column for total amount of words per email # had c(-1) in columns
 trainMatrix$wordcount = rowSums(trainMatrix)
@@ -90,15 +93,15 @@ nonextremevector$wordcount <- NULL
 # make vector of numerators for spam and not spam, do laplace smoothing
 # first entry is # of occurrences for word X0 in respective category
 sumextreme <- apply(extremevector, MARGIN = 2, sum) + 1
-sumnonextreme <- apply(nonextremevector, MARGIN = 2, sum) + 1
+sumnonextreme <- apply(nonextremevector, MARGIN = 2, sum) + laplace_smoother
 
 # make vectors
-phiextreme <- c(sumextreme/denomextreme)
-phinonextreme <- c(sumnonextreme/denomnonextreme)
+phiextreme <- c(sumextreme/(denomextreme + ncol(extremevector)))
+phinonextreme <- c(sumnonextreme/(denomnonextreme + ncol(extremevector)*laplace_smoother))
 
 
 
-i <- which((as.vector(sumextreme) > 1) & (as.vector(sumextreme + sumnonextreme - 2) > 4))
+#i <- which((as.vector(sumextreme) > 1) & (as.vector(sumextreme + sumnonextreme - 2) > 4))
 
 logratio <- phiextreme/phinonextreme
 logratio = sort(logratio, decreasing = TRUE)
@@ -139,8 +142,11 @@ for(i in 1:length(years)) {
   # get Laplace smoothing factor
   V = length(tokenlist_yr$X0)
   
-  # get prior y_hat MLE, # of spam emails divided by total
-  y_hat <- plyr::count(trainCategory_yr$EXTREME)$freq[2]/numTrain
+  # count documents
+  extreme_counts <- plyr::count(trainCategory_yr$EXTREME)
+  
+  # add laplace smoother for nonextreme events
+  laplace_smoother <- round(extreme_counts$freq[1]/extreme_counts$freq[2])
   
   # create a column for total amount of words per email # had c(-1) in columns
   trainMatrix_yr$wordcount = rowSums(trainMatrix_yr)
@@ -161,11 +167,11 @@ for(i in 1:length(years)) {
   # make vector of numerators for spam and not spam, do laplace smoothing
   # first entry is # of occurrences for word X0 in respective category
   sumextreme <- apply(extremevector, MARGIN = 2, sum) + 1
-  sumnonextreme <- apply(nonextremevector, MARGIN = 2, sum) + 1
+  sumnonextreme <- apply(nonextremevector, MARGIN = 2, sum) + laplace_smoother
   
   # make vectors
-  phiextreme <- c(sumextreme/denomextreme)
-  phinonextreme <- c(sumnonextreme/denomnonextreme)
+  phiextreme <- c(sumextreme/(denomextreme + ncol(extremevector)))
+  phinonextreme <- c(sumnonextreme/(denomnonextreme + ncol(extremevector)*laplace_smoother))
   
   
   ratio <- phiextreme/phinonextreme
@@ -181,18 +187,34 @@ for(i in 1:length(years)) {
 names(logratio[1:5])
   
 
-year_df %>% filter(token %in% names(logratio[1:12]) ) %>%  #.$token %>% unique
-  ggplot(aes(x = year, y = log(ratio), color = token)) +
-  geom_line(se = F) +
-  geom_point() +
-  geom_hline(yintercept = 0, linetype = 2) +
-  facet_wrap(~token) +
-  theme_bw()
+year_df %>% filter(token %in% names(logratio[1:10]) ) %>%  #.$token %>% unique
+  mutate(token = factor(toupper(token), levels = toupper(names(logratio[1:10])))) %>%
+  ggplot(aes(x = year, y = ratio)) +
+  geom_line() +
+  geom_point(size = 0.6) +
+  stat_smooth(geom = "line", se=F, method = "lm", color = "hotpink", size = 2, alpha = 0.5)+
+  geom_hline(yintercept = 1, linetype = 2) +
+  facet_wrap(~token, ncol = 5) +
+  theme_bw() +
+  labs(x = "Year", y = "Likelihood ratio of extreme injury")
 
 year_df %>% filter(year == 2016) %>% filter(token == "face")
 
 
-year_df
+year_df %>% 
+  group_by(year) %>%
+  mutate(rank = row_number()) %>%
+  ungroup() %>%
+  filter(token %in% names(logratio[1:10]) ) %>%  #.$token %>% unique
+  ggplot(aes(x = year, y = rank)) +
+  geom_line() +
+  stat_smooth(geom = "line", se=F, method = "lm", color = "hotpink", size = 2, alpha = 0.5)+
+  #geom_point() +
+  scale_y_reverse() +
+  #stat_smooth(geom = "line", se=F, method = "lm", color = "hotpink", size = 2, alpha = 0.5)+
+  #geom_hline(yintercept = 1, linetype = 2) +
+  facet_wrap(~token, ncol = 5) +
+  theme_bw()
 
 
 
@@ -201,7 +223,7 @@ year_df
 
 
 
-subunits <- MSHA2$SUBUNIT %>% unique
+subunits <- unique(MSHA1[MSHA1$SUBUNIT_CD %in% c(30,1,2,3,17, 6),9])$SUBUNIT
 
 subunit_df <- NULL
 
@@ -223,8 +245,11 @@ for(i in 1:length(subunits)) {
   # get Laplace smoothing factor
   V = length(tokenlist_yr$X0)
   
-  # get prior y_hat MLE, # of spam emails divided by total
-  y_hat <- plyr::count(trainCategory_yr$EXTREME)$freq[2]/numTrain
+  # count documents
+  extreme_counts <- plyr::count(trainCategory_yr$EXTREME)
+  
+  # add laplace smoother for nonextreme events
+  laplace_smoother <- round(extreme_counts$freq[1]/extreme_counts$freq[2])
   
   # create a column for total amount of words per email # had c(-1) in columns
   trainMatrix_yr$wordcount = rowSums(trainMatrix_yr)
@@ -245,36 +270,87 @@ for(i in 1:length(subunits)) {
   # make vector of numerators for spam and not spam, do laplace smoothing
   # first entry is # of occurrences for word X0 in respective category
   sumextreme <- apply(extremevector, MARGIN = 2, sum) + 1
-  sumnonextreme <- apply(nonextremevector, MARGIN = 2, sum) + 1
+  sumnonextreme <- apply(nonextremevector, MARGIN = 2, sum) + laplace_smoother
   
   # make vectors
-  phiextreme <- c(sumextreme/denomextreme)
-  phinonextreme <- c(sumnonextreme/denomnonextreme)
+  phiextreme <- c(sumextreme/(denomextreme + ncol(extremevector)))
+  phinonextreme <- c(sumnonextreme/(denomnonextreme + ncol(extremevector)*laplace_smoother))
   
   
   ratio <- phiextreme/phinonextreme
   ratio = sort(ratio, decreasing = TRUE)
   
-  df_ratio <- tibble(year = current_subunit, token = names(ratio), ratio = ratio)
+  df_ratio <- tibble(subunit = current_subunit, token = names(ratio), ratio = ratio)
   
   subunit_df <- rbind(subunit_df, df_ratio)
   
-  print(paste0("Year: ", current_subunit))
+  print(paste0("Subunit: ", current_subunit))
 }
 
 
 
-
-
 subunit_df %>% 
-  group_by(year) %>%
+  group_by(subunit) %>%
   slice_max(order_by = ratio, n = 5) %>%
   ungroup() %>%
-  ggplot(aes(x = fct_reorder(token, ratio), y = log(ratio))) +
+  ggplot(aes(x = fct_reorder(token, ratio), y = ratio)) +
   geom_col() +
-  facet_wrap(~year, scales = "free_y") +
+  facet_wrap(~subunit, scales = "free_y") +
   coord_flip()
   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+MSHA2 %>%
+  mutate(SUBUNIT = "Total") %>%
+  group_by(SUBUNIT) %>%
+  summarise(n = n(), extreme = sum(EXTREME)) %>%
+  ungroup() %>%
+  rbind(MSHA2 %>% filter(SUBUNIT %in% subunits) %>% group_by(SUBUNIT) %>% summarise(n = n(), extreme = sum(EXTREME))) %>%
+  rbind(MSHA2 %>% mutate(CAL_YR2 = ceiling((CAL_YR - 1999)/5)) %>% mutate(CAL_YR2 = ifelse(CAL_YR2 > 4, CAL_YR2-1, CAL_YR2)) %>%
+          group_by(CAL_YR2) %>% summarise(n = n(), extreme = sum(EXTREME)) %>% ungroup() %>% rename("SUBUNIT" = "CAL_YR2")
+          ) %>%
+  mutate(perc = round(extreme/n*100, 1)) %>%
+  mutate(extreme = paste0(extreme, " (", perc, "%)")) %>% select(-perc)
+
+
+
+
+logratio[1:10]
+
+logratio[(length(logratio) - 9):length(logratio)]
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
